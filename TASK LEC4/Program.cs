118d112022b84c42a16eb088748ef30bc114f207
wael <163440911+waelowis@users.noreply.github.com>
2025-03-08 22:11:14 +0300

    UPDATE

--- TASK LEC4/Program.cs
+++ TASK LEC4/Program.cs
@@ -1,10 +1,13 @@
 ﻿namespace TASK_LEC4
 {
+    // WAEL MOHAMED ABDOU OWIS
     internal class Program
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello, World!");
+
         }
     }
 }
